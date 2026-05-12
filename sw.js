ce81609ead2345a420fdc4f20d06511d68869c8d
mikahih2 <mikahih.2@gmail.com>
2026-05-11 22:24:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE = 'shift-v2';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE = 'shift-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
